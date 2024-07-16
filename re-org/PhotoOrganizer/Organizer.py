6196ecfcf0675fc20fed9c491190bbc15dca03b8
Subrahmanyam Gundimeda <subbu.klce@gmail.com>
2024-07-16 07:26:54 +0530

    committing files related to photo reorganizer

---
+++ re-org/PhotoOrganizer/Organizer.py
@@ -0,0 +1,8 @@
+import os
+
+class Organizer:
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+
+
---
+++ re-org/PhotoOrganizer/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from Organizer import Organizer as Og
+
+def print_hi(name):
+    obj = Og("Photo Organizer", 1.0)
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, { obj.name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
